6550116ecb1cfb07337c37a5842b999b895d5eb5
hachino.Y <149479436+hachinoY@users.noreply.github.com>
2023-12-14 10:44:46 +0000

    [add]詳細画面の表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,26 @@
 class BooksController < ApplicationController
-  def new
-     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
-    @books = Book.new
-    #@book = Book.all index作り終わった後で追加
-  end
-
   def create
     book = Book.new(book_params) #データを受け取り新規登録するためのインスタンス作成
     book.save#  データをデータベースに保存するためのsaveメソッド実行
-    redirect_to '/books/index'#  index画面へリダイレクト
-  end
-  
-  def index
-    @books = Book.all
-  end
-  
-   def destroy
-    book = @books.find(params[:id])
-    @books.destroy
-    respond_to do |book|
-      wants.html { redirect_to(model_class_names_url) }
-      wants.xml  { head :ok }
-    end
+    redirect_to book_path(book)#  show画面へリダイレクト
   end
 
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+   #book = @books.find(params[:id])
+    #@books.destroy
+    #respond_to do |book|
+     #wants.html { redirect_to(model_class_names_url) }
+      #wants.xml  { head :ok }
+    #end
+  #end 削除ぺージを後で追加
+
   def show
+    #詳細表示
+    @book = Book.find(params[:id])
   end
 
   def edit
