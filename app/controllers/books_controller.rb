4c420af16597fb880bfeb4c1f0e6cef054bb9cbe
hachino.Y <149479436+hachinoY@users.noreply.github.com>
2023-12-15 14:06:29 +0000

    [add]フラッシュメッセージの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,19 @@
 class BooksController < ApplicationController
+
   def create
-    book = Book.new(book_params) #データを受け取り新規登録するためのインスタンス作成
-    book.save#  データをデータベースに保存するためのsaveメソッド実行
-    redirect_to book_path(book)#  show画面へリダイレクト
+    #データを受け取り新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+       #データをデータベースに保存するためのsaveメソッド実行
+      if @book.save
+      #フラッシュメッセージを表示させる
+      flash[:notice] = "Book was successfully created."
+      #show画面へリダイレクト
+      redirect_to book_path(@book)
+      else
+      # フラッシュメッセージを定義し、index.html.erbを描画する
+      flash.now[:notice] = "投稿に失敗しました。"
+      render :new
+      end
   end
 
   def index
@@ -17,7 +28,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     #データを削除
     book.destroy
-    # 投稿一覧画面へリダイレクト
+    # showへリダイレクト
     redirect_to books_path
   end
 
@@ -32,9 +43,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(id:book.id)
+    #データを受け取る（一件）
+    @book = Book.find(params[:id])
+    #データを変更
+    @book.update(book_params)
+    # 投稿一覧画面へリダイレクト*なぜidがこの表記になるのかわからない
+    if @book.save
+    #フラッシュメッセージを表示させる
+    flash[:notice] = "Book was successfully created."
+    #show画面へリダイレクト
+    redirect_to book_path(id: @book.id)
+    else
+    # フラッシュメッセージを定義し、index.html.erbを描画する
+    flash.now[:notice] = "投稿に失敗しました。"
+    render :edit
+    end
   end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  # get 'books/new'
-  # post 'books' => 'books#create'
-  # get 'books/index'
-  # get 'book/:id' => 'books#destoroy'
-  # get 'books/show'
-  # get 'books/edit'
   root 'homes#top'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
